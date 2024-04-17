69fa2f59ec886f56dfabebff9c89f37f5f2850e2
XLZXLZ <1336764254@qq.com>
2024-04-17 22:04:54 +0800

    优化场景，设计入场动画，调整主菜单颜色

---
+++ Assets/C# Scripts/Title/TitleAnim.cs
@@ -0,0 +1,63 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
+using UnityEngine.UIElements;
+
+public class TitleAnim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform lightTitle;
+
+    [SerializeField]
+    private Transform blackTitle;
+
+    [SerializeField]
+    private Transform globalLight;
+
+    [SerializeField]
+    private float startInterval = 2f;
+
+    [SerializeField]
+    private float fallInterval = 0.1f;
+
+    [SerializeField]
+    private float moveTime = 2f;
+
+    [SerializeField]
+    private float shadowInterval = 6f;
+
+    [SerializeField]
+    private float shadowAppearTime = 0.5f;
+
+    private void Start() //屎山勿动
+    {
+        for (int i = 0; i < lightTitle.childCount; i++)
+        {
+            var child = lightTitle.GetChild(i);
+            for (int j = 0; j < child.childCount; j++)
+            {
+                var t = child.GetChild(j);
+
+                Vector3 origin = t.position;
+                float delay = t.position.y * fallInterval + startInterval;
+                t.position += Vector3.up * 20;
+                delay = Mathf.Max(0, delay);
+
+                DOTween.Sequence()
+                    .AppendInterval(delay)
+                    .Append(t.DOMove(origin, moveTime).SetEase(Ease.InQuart));
+            }
+        }
+
+        blackTitle.localScale -= Vector3.forward;
+        blackTitle.gameObject.SetActive(false);
+
+        DOTween.Sequence()
+            .AppendInterval(shadowInterval)
+            .Append(transform.DOMove(transform.position, 0).OnComplete(() => blackTitle.gameObject.SetActive(true))) //神秘代码
+            .Append(blackTitle.DOScale(Vector3.one, shadowAppearTime))
+            .Join(globalLight.DORotate(new Vector3(45, 180, 0), shadowAppearTime));
+    }
+}
